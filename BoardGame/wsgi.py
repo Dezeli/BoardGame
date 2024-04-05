fe6b0a02fd7d92003fe26595f8df2d36fd66ae98
Dezeli <haterecursive@gmail.com>
2024-04-05 13:54:42 +0900

    Develop User

--- BoardGame/wsgi.py
+++ BoardGame/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BoardGame.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BoardGame.settings")
 
 application = get_wsgi_application()
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = "user"
+
 urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("join/", views.join, name="join"),
+    path("login/", views.login_page, name="login"),
+    path("join/", views.join_page, name="join"),
+    path("logout/", views.logout_page, name="logout"),
 ]
